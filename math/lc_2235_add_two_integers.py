059d19386977e6a9ac1a45b5fc656d08e6a83b3f
Ridvik Pal <ridvikpal@gmail.com>
2025-12-12 23:33:41 -0500

    Added leetcode 2235

---
+++ math/lc_2235_add_two_integers.py
@@ -0,0 +1,7 @@
+'''
+https://leetcode.com/problems/add-two-integers/description/
+'''
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        # return the standard arithmetic sum
+        return num1+num2
