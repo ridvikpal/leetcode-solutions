fda7b878661036b0006b4c376c964ccf7688889e
Ridvik Pal <ridvikpal@gmail.com>
2025-12-15 22:40:54 -0500

    Added leetcode 121.

---
+++ leetcode/two_pointers/lc_121_best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+'''
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+'''
+class Solution:
+    # note this is a 2 pointer problem because
+    # we are inadvertently having 1 pointer point
+    # to the min buy price, and another pointing
+    # to the max sell price to get the max profit
+    def maxProfit(self, prices: List[int]) -> int:
+        # we will first init our
+        # buy price to (10^4)+1 because
+        # the max price possible is 10^4
+        buy_price = (10**4)+1
+
+        # we will init our max profit to 0
+        max_profit = 0
+
+        # loop through all prices
+        for i in range(len(prices)):
+            # we want to minimize our buy price
+            # if we find a lower buy price, then
+            # we will store that as our optimal buy price
+            buy_price = min(prices[i], buy_price)
+
+            # we wil calculate our profit as the difference
+            # between the buy price and the current price
+            profit = prices[i] - buy_price
+            # if the profit > max_profit, update max_profit
+            max_profit = max(profit, max_profit)
+
+        # finally, return the max profit
+        return max_profit
