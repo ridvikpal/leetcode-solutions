40dc5df7c55bdfe90a8577d61a02b4e81a442025
Ridvik Pal <ridvikpal@gmail.com>
2025-12-17 21:19:21 -0500

    Added leetcode 567 solution 2

--- leetcode/sliding_window/lc_567_permutation_in_string-sol-2.py
+++ leetcode/sliding_window/lc_567_permutation_in_string-sol-2.py
@@ -1,6 +1,3 @@
-from collections import Counter
-
-
 '''
 https://leetcode.com/problems/permutation-in-string/description/
 '''
@@ -13,10 +10,20 @@ class Solution:
             # in s2 is impossible, so return False
             return False
 
-        # first get mapping
-        # of each char -> it's count
-        # in the s1 string
-        s1_counter = Counter(s1)
+        # first we will create an array
+        # where each index corresponds to
+        # one of 26 lowercase letters
+        # and the value is the count those
+        # letters appear in s1
+        s1_count_array = [0] * 26
+
+        # then we will populate this array
+        # with the frequencies of chars in s1
+        # loop over all chars in s1
+        for char in s1:
+            # increment the respective frequencies
+            # in the s1 count array
+            s1_count_array[ord(char)-ord('a')] += 1
 
         # now we will begin checking
         # substrings that are of the
@@ -29,15 +36,25 @@ class Solution:
         # our fixed sliding window size is len(s1)
         end = len(s1)-1
         
-        # we can init our substring counter 
-        # to start with the mapping of each
-        # char -> it's count in the first substring
-        substring_counter = Counter(s2[start:end+1])
+        # we can create a similar array to map
+        # one of 26 lowercase letters as the index
+        # to the count of those letters in the value
+        # for our initial substring
+        substring_count_array = [0] * 26
 
+        # we can populate our substring count array
+        # with the char frequencies of the
+        # first initial substring 
+        # loop through all indices for the first substring
+        for i in range(start, end+1):
+            # increment the respective frequencies
+            # in the substring count array
+            substring_count_array[ord(s2[i])-ord('a')] += 1
+        
         # initially check if this first substring
         # is a permutation (meaning it has the same
         # char frequencies) of s1
-        if substring_counter == s1_counter:
+        if substring_count_array == s1_count_array:
             # if it does, then return True immediately
             return True
 
@@ -55,8 +72,7 @@ class Solution:
             # the leftmost char is removed from
             # the sliding window each time we shift it
             # from left to right
-            substring_counter[s2[start]] -= 1
-
+            substring_count_array[ord(s2[start])-ord('a')] -= 1
 
             # increment both the start and end pointers
             # moving our fixed sliding window 1 char to the right
@@ -68,13 +84,13 @@ class Solution:
             # the new sliding window, since the rightmost
             # char is the new char added to the sliding window
             # each time we shift it from left to right
-            substring_counter[s2[end]] += 1
+            substring_count_array[ord(s2[end])-ord('a')] += 1
 
             # check if this new substring from the new
             # sliding window is a permutation of s1
             # by checking if its char frequencies are the same
             # as the char frequencies of s1
-            if substring_counter == s1_counter:
+            if substring_count_array == s1_count_array:
                 # if they are, then return True immediately
                 return True
 
