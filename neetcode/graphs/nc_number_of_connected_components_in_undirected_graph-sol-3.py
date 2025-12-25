201b23a28a978138a229f91ee6608b6869f64fcb
Ridvik Pal <ridvikpal@gmail.com>
2025-12-25 08:16:58 -0500

    Added dsu with path compression and size for neetcode number of connected components in grpah

---
+++ neetcode/graphs/nc_number_of_connected_components_in_undirected_graph-sol-3.py
@@ -0,0 +1,110 @@
+from typing import List
+
+# We will use the disjoint set union 
+# (DSU aka. union-find) algorithm
+# with path compression and size
+# to solve this problem, so init the class
+class DSU():
+    def __init__(self, n):
+        # we can initialize our parent array
+        # this will hold the immediate parent of
+        # each node for each of our set trees
+        # ex [0, 1, 1, 3]
+        # this means that nodes 0 and 3 are their own
+        # parents (they are disjoint sets)
+        # while nodes 1 and 2 are part of the same set
+        # because they have the same parent, node 1
+        # we will perform this recursion to find the root
+        # parent in our find() function, and then path compression
+        # to ensure that each value in the parentArray directly
+        # holds the top level root parent, not the immediate root
+        self.parent = list(range(n))
+
+        # we can init our size array
+        # this will hold the number of connected nodes
+        # for each subtree, for each parent
+        # ex [1, 2, 1, 1]
+        # this means that node 2 is a parent node
+        # for which there are 2 nodes connected in it's subtree
+        # which from the parent array we can see are nodes 1 and 2
+        # however, nodes 0, 2 and 1 only have 1 node connected
+        # to their subtrees since they are only parents to themselves
+        # or are leaf child nodes for another parent
+        self.size = [1]*n
+
+        # we can initialize our component count.
+        # at the start, each node is it's own disjoint set
+        # and the component count is therefore n for n nodes
+        self.componentCount = n
+
+    # our find function that finds the root parent
+    # for a given node, recursing up the parent array
+    def find(self, node):
+        # if the current node is it's own parent
+        # then return it
+        if node == self.parent[node]:
+            return node
+        
+        # otherwise, recurse up the parent array
+        # until we find the root parent that is 
+        # it's own parent, and set that root parent
+        # as the node's parent in the parent array
+        # this is path compression
+        self.parent[node] = self.find(self.parent[node])
+        
+        # finally, just return the node's parent
+        return self.parent[node]
+
+    # our union function that joins two subsets
+    # (two subtrees) together if the input two nodes
+    # are from different subsets
+    def union(self, node1, node2):
+        # first find the parents of each node
+        parent1 = self.find(node1)
+        parent2 = self.find(node2)
+
+        # if the parents of each node are the same
+        # we have detected a cycle and so we can't
+        # combine the same sets together
+        if parent1 == parent2:
+            # just return without combining
+            return
+
+        # if the size of parent2 is > size of parent1
+        # swap them so we always merge sets where
+        # the set with most nodes "consumes" the set with
+        # less nodes in the lines below
+        if self.size[parent2] > self.size[parent1]:
+            parent1, parent2 = parent2, parent1
+        
+        # just set the parent2 node to be a child
+        # of parent1, thust moving all child nodes in parent2
+        # to be child nodes of parent1
+        # from above, we guarentee that size(parent1) > size(parent2)
+        self.parent[parent2] = parent1
+
+        # after merging parent2 into parent1, we can increment the size
+        # of parent 1 by the size of parent2 because it now contains 
+        # those new child nodes
+        self.size[parent1] += self.size[parent2]
+
+        # since we merged two sets together, we can decrement
+        # the component count, since there is 
+        # 1 less connected component (1 less disjoint set)
+        self.componentCount -= 1
+
+'''
+https://neetcode.io/problems/count-connected-components/question
+'''
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        # we will init our dsu object
+        dsu = DSU(n)
+
+        # loop through all edges
+        for start, end in edges:
+            # perform union on each node in each edge
+            dsu.union(start, end)
+
+        # finally, just return the component count
+        return dsu.componentCount
