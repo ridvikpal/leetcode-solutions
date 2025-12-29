6c30817e54343067e159db3208cecf0d204a226f
Ridvik Pal <ridvikpal@gmail.com>
2025-12-28 22:44:42 -0500

    Updated count solutions script

--- count_solutions.sh
+++ count_solutions.sh
@@ -2,7 +2,9 @@
 
 find . -type f \
     -not -path "./.git/*" \
+    -not -path "./.vscode/*" \
     -not -path "./README.md" \
     -not -path "./count_solutions.sh" \
+    -not -path "./.gitignore" \
     -not -path "./LICENSE" \
     | wc -l
