8e60ed89c1fb6b636128ea1c9d4fe94b4d724a55
Ridvik Pal <ridvikpal@gmail.com>
2025-12-30 19:57:17 -0500

    Added 2 other solutions for coin change

---
+++ leetcode/dynamic_programming/lc_322_coin_change-sol-3.py
@@ -0,0 +1,65 @@
+import math
+from typing import List
+
+
+'''
+https://leetcode.com/problems/coin-change/description/
+'''
+class Solution:
+    # we will use a top down dynamic programming approach
+    # by memoizing a recursive function to solve this problem
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # init our memoization dict
+        # we need a dict because our keys
+        # will be tuples (index, amount)
+        memoization = dict()
+
+        # create our recursive (backtracking) function
+        # it will find the minimum number of coins to create
+        # a given amount
+        def search(index, amount):
+            # our first base case is when
+            # our amount is 0. This means we
+            # have a valid solution
+            if amount == 0:
+                # we can simply return 0,
+                # since we are not selecting any more coins
+                return 0
+
+            # our second base case is when
+            # our amount < 0 or the index is
+            # out of bounds. This means we have 
+            # an invalid solution
+            if amount < 0 or index >= len(coins):
+                # we can return infinity to indicate
+                # that there is no minimum number of
+                # coins along this combination 
+                return math.inf
+
+            # check if we already have a memoized value stored
+            if (index, amount) in memoization:
+                # if we do, then return that value
+                return memoization[(index, amount)]
+            
+            # we can recursively explore all combinations
+            # by testing 2 decision:
+            # 1) choosing this coin, so add 1 to the recursion stack
+            #    and reduce amount by this coin value
+            # 2) skipping this coin, so just go to the next index
+            choose_this_coin = 1 + search(index, amount-coins[index])
+            skip_this_coin = search(index+1, amount)
+
+            # we will add the value of the decision that gives
+            # the minimum answer to our memoization dict
+            memoization[(index, amount)] = min(choose_this_coin, skip_this_coin)
+
+            # finally we can return our memoization value
+            return memoization[(index, amount)]
+
+        # we will call our search function with
+        # starting index 0 and amount
+        min_coins = search(0, amount)
+
+        # we will return the minimum coins or we will return 
+        # -1 if our search function returns infinity
+        return min_coins if min_coins != math.inf else -1
