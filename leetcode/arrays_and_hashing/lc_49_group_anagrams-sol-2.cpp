8ac3af9d27e3acfff5530e67a931cf37e0af894d
Ridvik Pal <ridvikpal@gmail.com>
2026-03-22 23:10:09 -0400

    Added second solution for leetcode 49

---
+++ leetcode/arrays_and_hashing/lc_49_group_anagrams-sol-2.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+/**
+ * https://leetcode.com/problems/group-anagrams/description/
+ */
+class Solution {
+public:
+    std::vector<int> countChars(const std::string& str) {
+        std::vector<int> counter(26, 0);
+
+        for (char chr: str) {
+            counter[chr - 'a'] += 1;
+        }
+
+        return counter;
+    }
+
+    std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs) {
+        std::vector<std::vector<std::string>> result;
+        std::unordered_map<std::string, std::vector<std::string>> anagramMap;
+
+        for (const std::string& str: strs) {
+            std::vector<int> counter = countChars(str);
+
+            std::string key = "";
+
+            for (const int& count: counter) {
+                key += std::to_string(count) + '#';
+            }
+
+            anagramMap[key].push_back(str);
+        }
+
+        for (const auto& anagramMapPair: anagramMap) {
+            result.push_back(anagramMapPair.second);
+        }
+
+        return result;
+    }
+};
