3aec7d57f179a5c283dc427545a6e748d9703cd7
Ridvik Pal <ridvikpal@gmail.com>
2026-03-22 20:05:41 -0400

    Added solution for lc 242 cpp

---
+++ leetcode/arrays_and_hashing/lc_242_valid_anagram.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <unordered_map>
+
+class Solution {
+public:
+    bool isAnagram(std::string s, std::string t) {
+        std::unordered_map<char, int> s_counter;
+        std::unordered_map<char, int> t_counter;
+
+        for (char s_char: s) {
+            s_counter[s_char] += 1;
+        }
+
+        for (char t_char: t) {
+            t_counter[t_char] += 1;
+        }
+
+        if (s_counter == t_counter) {
+            return true;
+        }
+
+        return false;
+    }
+};
