e53e1a9b11830f678b6ca44f3be774216e377ba8
Ridvik Pal <ridvikpal@gmail.com>
2026-04-14 13:21:54 -0400

    Added comments for cpp group anagrams solution

--- leetcode/cpp/arrays_and_hashing/lc_49_group_anagrams-sol-1.cpp
+++ leetcode/cpp/arrays_and_hashing/lc_49_group_anagrams-sol-1.cpp
@@ -12,20 +12,33 @@
 class Solution {
 public:
     std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs) {
+        // init our result 2d array, where each element will be
+        // an array containing anagrams
         std::vector<std::vector<std::string>> result;
+        // init our anagram map which maps sorted strings
+        // to array of strings which are anagrams of the sorted strings
         std::unordered_map<std::string, std::vector<std::string>> anagramMap;
 
+        // loop through all given strings
         for (const std::string& str: strs) {
+            // copy the current string into a new string str_key
+            // this will be the key for the anagram map
             std::string str_key = str;
+            // sort the str_key
             std::sort(str_key.begin(), str_key.end());
 
+            // add the current string to the anagram map
+            // with the str_key as the key
             anagramMap[str_key].push_back(str);
         }
 
+        // loop through all key, value pairs in the anagram map
         for (const auto& anagramMapPair : anagramMap) {
+            // add each array of anagrams to the results array
             result.push_back(anagramMapPair.second);
         }
 
+        // finally return the result array
         return result;
     }
 };
