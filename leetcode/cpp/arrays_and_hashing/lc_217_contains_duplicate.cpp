a52a462440c41259bd273aefcc46768590e9260e
Ridvik Pal <ridvikpal@gmail.com>
2026-04-18 10:36:33 -0400

    Added comments for lc 217, lc 242 cpp

--- leetcode/cpp/arrays_and_hashing/lc_217_contains_duplicate.cpp
+++ leetcode/cpp/arrays_and_hashing/lc_217_contains_duplicate.cpp
@@ -10,15 +10,23 @@
 class Solution {
 public:
     bool containsDuplicate(std::vector<int>& nums) {
+        // create our hashset to store numbers we have seen before
         std::unordered_set<int> seen;
 
+        // loop through all numbers
         for (const int& num : nums) {
             if (seen.find(num) != seen.end()) {
+                // if we have seen this number before
+                // return true
                 return true;
             }
+            
+            // add each number to our seen hashset
             seen.insert(num);
         }
 
+        // if we get through all numbers and don't find
+        // a duplicate we can return false
         return false;
     }
 };
--- leetcode/cpp/arrays_and_hashing/lc_242_valid_anagram.cpp
+++ leetcode/cpp/arrays_and_hashing/lc_242_valid_anagram.cpp
@@ -10,21 +10,34 @@
 class Solution {
 public:
     bool isAnagram(std::string s, std::string t) {
+        // create our hashamp to store the count of
+        // each char in string s
         std::unordered_map<char, int> s_counter;
+        // create our hashamp to store the count of
+        // each char in string t
         std::unordered_map<char, int> t_counter;
 
+        // loop through all chars in string s
         for (const char& s_char : s) {
+            // increment the count of this char
+            // in our s_counter hashmap
             s_counter[s_char] += 1;
         }
 
+        // loop through all chars in string t
         for (const char& t_char : t) {
+            // increment the count of this char
+            // in our t_coutner hashmap
             t_counter[t_char] += 1;
         }
 
         if (s_counter == t_counter) {
+            // if the two counters contain the same values
+            // they are anagrams so return true
             return true;
         }
 
+        // else we can return because they are not anagrams
         return false;
     }
 };
